c1e75d4909548a7a06a4d485256d632675bd1e75
theresareese <theresaareese@gmail.com>
2020-02-01 13:16:45 -0500

    adding draw methods

--- CameraView.py
+++ CameraView.py
@@ -1,10 +1,15 @@
 class CameraView:
 	def __init__(self):
-		x = 1
+		self.player_image_1 = pygame.image.load('images/ufo.png')
+		self.player_image_2 = pygame.image.load('images/ufo.png')
+		self.trash_image = pygame.image.load('images/enemy.png')
 
-	def processInput(self, screen, logic, dt):
+	def processInput(self, window, logic, dt):
 		pass
 
-	def draw(self, screen, logic):
-		pass
+	def draw(self, window, logic):
+		window.blit(self.player_image_1,(logic.p1.pos[0], logic.p1.pos[1]))
+		window.blit(self.player_image_2,(logic.p2.pos[0], logic.p2.pos[1]))
+		for trash in logic.trash_list:
+			window.blit(self.trash_image,(trash.pos[0], trash.pos[1]))
 
